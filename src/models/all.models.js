4e063a3b547120b85202c60a2bdd9a8b8c3b4a5c
Kiran <Kiranpatil73827@gmail.com>
2025-03-05 16:26:27 +0530

    Added games model

--- src/models/all.models.js
+++ src/models/all.models.js
@@ -1,6 +1,24 @@
 import mongoose,{Schema} from "mongoose";
 
 const GamesSchema=new Schema({
+    name:{
+        unique:true,
+        required:true,
+        type:String
+    },
+    genre:{
+        type:String,
+        required:true,
+    },
+    rating:{
+        type:Number,
+        required:true,
+        max:100,
+        min:1
+    }
+    
 
+},{timestamps:true})
 
-})
+export const Games=mongoose.models("Games",GamesSchema)
+
