2dbd0b47b2dfd6ef030e57402031bcf7ee255f10
Kiran <Kiranpatil73827@gmail.com>
2025-03-07 15:26:55 +0530

    Question2 is done

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from "express"
 import { Gameroute } from "./Routes/Gameroutes.js"
+import { q2router } from "./Routes/q2.routes.js"
+
 
 const app=express()
 
 app.use(express.json())
 
 app.use("/api/q1/games",Gameroute)
+app.use("/api/q2",q2router)
+
 
 export {app}
